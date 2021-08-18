2e50f06cf829760f0381dc6f1a8c9274bc6c399e
Bello Yakub <hyaaquub@gmail.com>
2021-08-18 02:30:41 +0100

    completed compulsory task

---
+++ compulsory.java
@@ -0,0 +1,10 @@
+class Compulsory{
+
+    public static void main(String[] args) {
+
+    System.out.println("I am Bello Yakub \n" + 
+    "A HNG intern with hyaaquub@gmail slack email.\n"+
+    "It's so nice to be here");
+        
+    }
+}
